5818d7b5e64852a1e389d1a2b02bafe88cd75ddc
Raymon Roos <raymon.roos@hotmail.com>
2025-02-11 21:21:26 +0100

    feat(04): completed chapter about traits
    I think it's good to spend some time familiarizing oneself with these
    commonly occurring traits. It's pretty cool how you can "unlock" useful
    behaviour by implementing existing traits for your types. And sometimes
    you don't even need any code for it, but just a `#(Derive[])`.
    
    I still don't understand associated types at all though.

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,74 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl SaturatingU16 {
+    fn new(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self::new(value)
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        Self::from(*value)
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self::new(value as u16)
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        Self::from(*value)
+    }
+}
+
+impl Add for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        self + rhs.value
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &Self) -> Self::Output {
+        self + *rhs
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: u16) -> Self::Output {
+        Self::new(self.value.saturating_add(rhs))
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u16) -> Self::Output {
+        self + *rhs
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
